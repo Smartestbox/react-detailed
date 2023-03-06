3cd64657aac3f1da617f7d81a17c3530872b0a66
Igor Olkhov <smartestbox@gmail.com>
2023-03-06 02:20:02 +0100

    Add Accordion component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Accordion from "./components/Accordion/Accordion";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <AppTitle />
+            <Rating />
+            <Accordion />
+        </div>
+    );
+}
+
+const AppTitle = () => {
+    return (
+        <>
+            App component
+        </>
+    )
+}
+
+const Star = () => {
+    return (
+        <div>
+            <div>star</div>
+        </div>
+    )
+}
+const Rating = () => {
+    return (
+        <div>
+            <Star />
+            <Star />
+            <Star />
+            <Star />
+            <Star />
+        </div>
+    )
 }
 
 export default App;
